faec04ee222a421efeb1dc5872f264d21991077e
Memocana <mehmetalaca@lavvlan5060705.apng.wireless-pennnet.upenn.edu>
2016-12-05 20:36:33 -0500

    Full Site

---
+++ js/templatemo_script.js
@@ -0,0 +1,112 @@
+var top_menu_height = 0;
+jQuery(function($) {
+		$(window).load( function() {
+			$('.external-link').unbind('click');	
+		});
+		
+        $(document).ready( function() {
+
+            // load google map
+        var script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&' +
+            'callback=initialize';
+        document.body.appendChild(script);
+
+        top_menu_height = $('.templatemo-top-menu').height();
+        // scroll spy to auto active the nav item
+        $('body').scrollspy({ target: '#templatemo-nav-bar', offset: top_menu_height + 10 });
+		$('.external-link').unbind('click');
+
+        // scroll to top
+        $('#btn-back-to-top').click(function(e){
+            e.preventDefault();
+            scrollTo('#templatemo-top');
+        });
+
+        // scroll to specific id when click on menu
+        $('.templatemo-top-menu .navbar-nav a').click(function(e){
+            e.preventDefault(); 
+            var linkId = $(this).attr('href');
+            scrollTo(linkId);
+            if($('.navbar-toggle').is(":visible") == true){
+                $('.navbar-collapse').collapse('toggle');
+            }
+            $(this).blur();
+            return false;
+        });
+
+        // to stick navbar on top
+        $('.templatemo-top-menu ').stickUp();
+
+        // gallery category
+        $('.templatemo-gallery-category a').click(function(e){
+            e.preventDefault(); 
+            $(this).parent().children('a').removeClass('active');
+            $(this).addClass('active');
+            var linkClass = $(this).attr('href');
+            $('.gallery').each(function(){
+                if($(this).is(":visible") == true){
+                   $(this).hide();
+                };
+            });
+            $(linkClass).fadeIn();  
+        });
+
+        //gallery light box setup
+        $('a.colorbox').colorbox({
+                                    rel: function(){
+                                        return $(this).data('group');
+
+                                    }
+        });
+    });
+});
+
+
+
+function initialize() {
+    var mapOptions = {
+      zoom: 16,
+      center: new google.maps.LatLng(41.17614, 28.89188),
+      scrollwheel: false
+    };
+
+   
+
+    var map = new google.maps.Map(document.getElementById('map-canvas'),  mapOptions);
+
+    var marker = new google.maps.Marker({
+      position: new google.maps.LatLng(41.17614, 28.89188),
+      map: map,
+      title: 'Hisar Okulları'
+  });
+    var contentString = '<div id="content">'+
+      '<div id="siteNotice">'+
+      '</div>'+
+      '<h1 id="firstHeading" class="firstHeading">Hisar Okulları</h1>'+
+      '<div id="bodyContent">'+
+      '<p><b>Hisar Okulları</b>, Göktürk Merkez Mahallesi İstanbul Caddesi No:3 Eyüp/İstanbul 34077</p>'+
+      '</div>'+
+      '</div>';
+
+  var infowindow = new google.maps.InfoWindow({
+      content: contentString
+  });
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.open(map,marker);
+  });
+
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
+// scroll animation 
+function scrollTo(selectors)
+{
+
+    if(!$(selectors).size()) return;
+    var selector_top = $(selectors).offset().top - top_menu_height;
+    $('html,body').animate({ scrollTop: selector_top }, 'slow');
+
+}
